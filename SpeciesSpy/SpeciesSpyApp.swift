674c979c6a19328f75b92a307b87f7ff6f5dae5f
John Newman <john.newman920@gmail.com>
2025-09-02 21:30:02 +1000

    Initial Commit

---
+++ SpeciesSpy/SpeciesSpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeciesSpyApp.swift
+//  SpeciesSpy
+//
+//  Created by John Newman on 2/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpeciesSpyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
